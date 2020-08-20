f29c77bdf6cbb5674d3242baa41cc1a0fb18b3a3
Jdowdy <Jdowdy9k@gamil.com>
2020-08-20 14:15:10 -0400

    got api data to print on page

--- src/components/Results/Result.js
+++ src/components/Results/Result.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function Result({ result }) {
-    console.log(result);
+    console.log(result.toJS()._highlightResults);
     return (
         <>
+           <div>
+               <h1> {result.toJS().title}</h1>
+               <h2> {result.toJS().url}</h2>
+               <h2> {result.toJS().author}</h2>
+           </div>
         </>
     )
 }
