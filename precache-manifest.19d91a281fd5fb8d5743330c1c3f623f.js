a6bb66cc3fdaa5409260536fe4b29140417738d2
Justin Dowdy <Jdowdy9k@gmail.com>
2020-08-31 14:20:33 -0400

    Updates

---
+++ precache-manifest.19d91a281fd5fb8d5743330c1c3f623f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d951e64c1e7632d8b8b6869f7ad29522",
+    "url": "/Hacker-News/index.html"
+  },
+  {
+    "revision": "f78ac7a270a45f479efa",
+    "url": "/Hacker-News/static/js/2.b6ae8404.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Hacker-News/static/js/2.b6ae8404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c4e8ce5ccc9452c711",
+    "url": "/Hacker-News/static/js/main.72336b28.chunk.js"
+  },
+  {
+    "revision": "d83e22c4c32c3241f1f3",
+    "url": "/Hacker-News/static/js/runtime-main.780eaefa.js"
+  }
+]);
